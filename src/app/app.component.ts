98574164c66c193df6e25c6fc6a3fa7020b7f442
Murilo Abreu <murilo_abr@yahoo.com.br>
2022-01-27 17:49:02 -0300

    1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,VERSION } from '@angular/core';
+
+@simplesDecorator({value1: 5, value2: 7})
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DXS | APP ANGULAR';
+  title = '';
+  constructor() {
+    console.log('Passando pelo constructor() da CLASSE');
+  }
+ 
+  ngOnInit() {
+    console.log('VALOR 1 : ' + (this as any).value1);
+    console.log('VALOR 2 : ' + (this as any).value2);
+    this.title = 'DXS | APP ANGULAR: ' + VERSION.full;
+  }  
 }
+
+function simplesDecorator(args: any) {
+  console.log('ARGUMENTOS DO DECORADOR: '+ JSON.stringify(args));
+ 
+  return function(target: any) {
+    console.log('Passando pelo metodo Decorator');
+    console.log('TIPO: ' + typeof target);
+    console.log('ALVO: ' + target);
+ 
+    Object.defineProperty(target.prototype, 'value1', {
+      value: args.value1 + 10,
+      writable: false
+    });
+ 
+    Object.defineProperty(target.prototype, 'value2', {
+      value: args.value2 + 20,
+      writable: false
+    });
+  };  
+}
+  
